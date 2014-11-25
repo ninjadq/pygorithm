f3ae8085c8a90a4e3a37b1b525a99704fa417a5b
Qian Deng <dengqian0826@gmail.com>
2014-11-25 21:15:03 +0800

    add fix bug of max pq and add minpq

--- pq.py
+++ pq.py
@@ -1,36 +1,81 @@
 class MaxPQ(object):
     def __init__(self, lst = None):
-        self.pq = []
+        self.pq = [None]
+        self.N = 0
         if lst is not None:
-            for i in lst: self.insert()
+            for i in lst: self.insert(i)
 
     def insert(self, v):
         self.pq.append(v)
-        self.__swim(len(self.pq)-1)
+        self.N += 1
+        self.__swim(self.N)
 
     def delMax(self):
-        m = self.pq[0]
-        self.pq[0], self.pq[len(self.pq)-1] = self.pq[len(self.pq)-1], self.pq[0]
-        del(self.pq[len(self.pq)-1])
-        self.__sink(0)
-        return m
+        if self.isEmpty(): raise ValueError
+        max_value = self.pq[1]
+        self.pq[1], self.pq[self.N] = self.pq[self.N], self.pq[1]
+        del(self.pq[self.N])
+        self.N -= 1
+        self.__sink(1)
+        return max_value
 
     def isEmpty(self):
-        return len(self.pq) == 0
+        return self.N == 0
 
     def size(self):
-        return len(self.pq)
+        return self.N
 
     def __swim(self, index):
-        while index != 0 and self.pq[index//2] < self.pq[index]:
+        while index > 1 and self.pq[index//2] < self.pq[index]:
             self.pq[index//2], self.pq[index] = self.pq[index], self.pq[index//2]
             index = index//2
 
     def __sink(self, index):
-        while 2*index < len(self.pq):
+        while 2*index <= self.N:
             j = 2*index
-            if j < len(self.pq)-1 and self.pq[j] < self.pq[j+1]: j += 1
+            if j < self.N and self.pq[j] < self.pq[j+1]: j += 1
             if self.pq[index] >= self.pq[j]: break;
             self.pq[index], self.pq[j] = self.pq[j], self.pq[index]
             index = j
 
+
+class MinPQ(object):
+
+    def __init__(self, lst = None):
+        self.pq = [None]
+        self.N = 0
+        if lst is not None:
+            for i in lst: self.insert(i)
+
+    def insert(self, v):
+        self.pq.append(v)
+        self.N += 1
+        self.__swim(self.N)
+
+    def delMin(self):
+        if self.isEmpty(): raise ValueError
+        min_value = self.pq[1]
+        self.pq[1], self.pq[self.N] = self.pq[self.N], self.pq[1]
+        del(self.pq[self.N])
+        self.N -= 1
+        self.__sink(1)
+        return min_value
+
+    def isEmpty(self):
+        return self.N == 0
+
+    def size(self):
+        return self.N
+
+    def __swim(self, index):
+        while index > 1 and self.pq[index//2] > self.pq[index]:
+            self.pq[index//2], self.pq[index] = self.pq[index], self.pq[index//2]
+            index = index//2
+
+    def __sink(self, index):
+        while 2*index <= self.N:
+            j = 2*index
+            if j < self.N and self.pq[j] > self.pq[j+1]: j += 1
+            if self.pq[index] <= self.pq[j]: break;
+            self.pq[index], self.pq[j] = self.pq[j], self.pq[index]
+            index = j
--- test_pq.py
+++ test_pq.py
@@ -1,7 +1,7 @@
 import pq
 import unittest
 
-class TestPQ(unittest.TestCase):
+class TestMaxPQ(unittest.TestCase):
     
     def setUp(self):
         self.mpq = pq.MaxPQ()
@@ -20,7 +20,41 @@ class TestPQ(unittest.TestCase):
         self.mpq.insert(1)
         self.assertEqual(self.mpq.size(), 5)
         self.assertEqual(self.mpq.delMax(), 6)
+        self.assertEqual(self.mpq.delMax(), 5)
+        self.assertEqual(self.mpq.size(), 3)
+    def test_pq_with_initializer(self):
+        self.mpq = pq.MaxPQ([3,4,5,6,7,2,3,1])
+        self.assertEqual(self.mpq.delMax(), 7)
+        self.assertEqual(self.mpq.delMax(), 6)
+        self.assertEqual(self.mpq.size(), 6)
+
+class TestMinPQ(unittest.TestCase):
+    def setUp(self):
+        self.mpq = pq.MinPQ()
+
+    def tearDown(self):
+        self.mpq = None
+
+    def test_pq_function(self):
+        self.assertTrue(self.mpq.isEmpty())
+        self.assertEqual(self.mpq.size(), 0)
+        self.mpq.insert(3)
+        self.assertFalse(self.mpq.isEmpty())
+        self.assertEqual(self.mpq.size(), 1)
+        self.mpq.insert(5)
+        self.mpq.insert(2)
+        self.mpq.insert(6)
+        self.mpq.insert(1)
+        self.assertEqual(self.mpq.size(), 5)
+        self.assertEqual(self.mpq.delMin(), 1)
         self.assertEqual(self.mpq.size(), 4)
+
+    def test_pq_with_initializer(self):
+        self.mpq = pq.MinPQ([3,4,5,6,7,2,3,1])
+        self.assertEqual(self.mpq.delMin(), 1)
+        self.assertEqual(self.mpq.delMin(), 2)
+        self.assertEqual(self.mpq.size(), 6)
+
 
 if __name__ == '__main__':
     unittest.main()
